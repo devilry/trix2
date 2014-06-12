5fbc500885a116ada8a78300dbbd63172cae856c
Espen Angell Kristiansen <post@espenak.net>
2014-06-12 18:04:53 +0200

    project.default|requirements.develop: Added django-debug-toolbar.

--- trix/project/default/settings.py
+++ trix/project/default/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'south',
+    'debug_toolbar',
     'crispy_forms',
     'django_cradmin',
     'trix.trix_core',
@@ -52,6 +53,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 
