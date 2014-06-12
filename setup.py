1fc6c42e5b3c239af174c041e3b89bf6e8284bcc
Espen Angell Kristiansen <post@espenak.net>
2014-06-12 12:27:49 +0200

    Added MANIFEST.in, setup.py and the root module for the project.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+execfile('django_dbdev/version.py')
+
+setup(
+    name='trix2',
+    description='Trix2.',
+    version=version,
+    url='https://gifthub.com/devilry/trix2',
+    author='Tor Johansen, Espen Angell Kristiansen',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'Django'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
