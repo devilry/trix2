32f9d22385a350b0261bd0dcd76a3db493c2f0f9
Tor Ivar Johansen <tor@torjohansen.no>
2014-06-13 17:01:32 +0200

    trix_student: Translations

---
+++ trix/project/develop/fabrictasks/i18n.py
@@ -0,0 +1,29 @@
+from fabric.api import local, task
+
+from .common import root_develop_managepy
+
+
+@task
+def makemessages(langcode='nb'):
+    """
+    Runs /path/to/reporoot/manage.py makemessages for the given locale (default to nb).
+    """
+    root_develop_managepy('makemessages -l {} -i static/*'.format(langcode))
+
+@task
+def jsmakemessages(langcode='nb'):
+    """
+    Runs /path/to/reporoot/manage.py makemessages for javascript in the given locale (default to nb).
+    """
+    root_develop_managepy(
+        ('makemessages -d djangojs -l {} -v3 '
+         '-i "static/**/node_modules/*" '
+         '-i "static/**/bower_components/*"').format(langcode))
+
+
+@task
+def compilemessages():
+    """
+    Runs /path/to/reporoot/manage.py compilemessages.
+    """
+    root_develop_managepy('compilemessages')
