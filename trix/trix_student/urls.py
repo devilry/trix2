6e33515b2b38b73a797ac6f04937657a3f0c3c16
Tor Ivar Johansen <toij@ifi.uio.no>
2014-06-23 20:37:25 +0200

    trix_student: Simple assignment list view

--- trix/trix_student/urls.py
+++ trix/trix_student/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from trix.trix_student.views import dashboard
+from trix.trix_student.views import assignments
 
 urlpatterns = patterns('trix',
-    url('^$', dashboard.CourseListView.as_view(), name='trix_student_dashboard' )
+    url('^$', dashboard.CourseListView.as_view(), name='trix_student_dashboard' ),
+    url('^assignments/$', assignments.AssignmentListView.as_view(), name='trix_student_assignments')
 
 
     )
---
+++ trix/trix_student/views/assignments.py
@@ -0,0 +1,6 @@
+from django.views.generic import ListView
+from trix.trix_core import models
+
+class AssignmentListView(ListView):
+    model = models.Assignment
+    template_name = "trix_student/assignments.django.html"
