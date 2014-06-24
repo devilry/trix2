6d77c31d0766f7fd3d3829993eaca8fde71bc781
Tor Ivar Johansen <toij@ifi.uio.no>
2014-06-24 22:19:27 +0200

    trix_student: Using query string not complete

--- trix/trix_student/views/course.py
+++ trix/trix_student/views/course.py
@@ -1,11 +1,22 @@
 from django.views.generic import DetailView
 from trix.trix_core import models
 
+from django import forms
+
+class TagSelectForm(forms.Form):
+    tag_choice = forms.ModelChoiceField(queryset=None)
+
+    def __init__(self, *args, **kwargs):
+        choices = kwargs.pop('choices')
+        super(TagSelectForm, self).__init__(*args, **kwargs)
+        self.fields['tag_choice'].queryset = choices
 
 class CourseDetailView(DetailView):
     model = models.Course
     pk_url_kwarg = 'course_id'
     template_name = "trix_student/course.django.html"
+
+    selected_tags = None
 
     def get_context_data(self, **kwargs):
         context = super(CourseDetailView, self).get_context_data(**kwargs)
@@ -13,4 +24,27 @@ class CourseDetailView(DetailView):
         assignments = models.Assignment.objects.filter_by_tag(obj.course_tag)\
             .filter_by_tag(obj.active_period)
         context['assignment_list'] = assignments
+
+        tags = models.Tag.objects.filter(assignment__in=assignments).distinct()
+        tags = tags.exclude(id__in=[x.id for x in (obj.course_tag, obj.active_period)])
+        context['tags'] = tags
+
+
+        tag_query_list = self.request.GET.get('locked_tags', None)
+        context['locked_tags'] = [obj.active_period, obj.course_tag]
+        if tag_query_list:
+            for tag in tag_query_list:
+                context['locked_tags'].append(models.Tag.objects.get(id=int(tag)))
+
+        tag_param = self.request.GET.get('tag_choice', None)
+        if tag_param:
+            selected_tag = models.Tag.objects.get(id=int(tag_param))
+            context['locked_tags'].append(selected_tag)
+            context['assignment_list'] = assignments.filter_by_tag(selected_tag)
+
+        context['locked_tags'] = list(set(context['locked_tags']))
+        print context['locked_tags']
+
+        context['tag_select_form'] = TagSelectForm(choices=tags)
         return context
+
