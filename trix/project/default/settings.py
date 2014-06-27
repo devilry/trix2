5ffef3ab599fba9931ad49da413151afe08009c7
Espen Angell Kristiansen <post@espenak.net>
2014-06-27 03:46:49 +0200

    Set language to nb.

--- trix/project/default/settings.py
+++ trix/project/default/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'nb'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
