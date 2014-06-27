a20ea90840e4a310837819c710c9348d9eb20c00
Espen Angell Kristiansen <post@espenak.net>
2014-06-27 03:54:21 +0200

    project.default.settings: Default language en-us.

--- trix/project/default/settings.py
+++ trix/project/default/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'nb'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
