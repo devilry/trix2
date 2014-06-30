6a0aa2f94e11a9b8401edaba90ced1770039feb6
Tor Ivar Johansen <toij@ifi.uio.no>
2014-06-30 17:09:54 +0200

    trix_student: Post handling

--- trix/trix_student/urls.py
+++ trix/trix_student/urls.py
@@ -9,7 +9,7 @@ from trix.trix_student.views import howsolved
 urlpatterns = patterns('trix',
     url('^$', dashboard.CourseListView.as_view(), name='trix_student_dashboard' ),
     url('^assignments/$', assignments.AssignmentListView.as_view(), name='trix_student_assignments'),
-    url('^assignments/howsolved/(?P<assignment_id>\d+)$', howsolved.HowsolvedView.as_view(), name='trix_student_howsolved'),
+    url('^assignment/howsolved/$', howsolved.HowsolvedView.as_view(), name='trix_student_howsolved'),
     url('^course/(?P<course_id>\d+)$', course.CourseDetailView.as_view(), name='trix_student_course'),
     url(r'^login$', 'trix_student.views.login.loginview', name='trix-login'),
     url(r'^logout$', 'trix_student.views.logout.logoutview', name='trix-logout'),
--- trix/trix_student/views/howsolved.py
+++ trix/trix_student/views/howsolved.py
@@ -1,5 +1,31 @@
 from django.views.generic import View
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from trix.trix_core import models
+
+import json
+
 
 class HowsolvedView(View):
     """docstring for UpdateHowSolvedView"""
     http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        howsolved = request.POST.get('howsolved', None)
+        self.assignment_id = request.POST.get('assignment_id', None)
+
+        self.assignment = get_object_or_404(models.Assignment, id=self.assignment_id)
+        self.assignment_solution = self.assignment.assignment_set\
+                .filter(id=self.assignment.id)\
+                .filter(user=request.user)
+
+        if self.assignment_solution:
+            self.assignment_solution.howsolved = self.howsolved
+            self.assignment_solution.save()
+        else:
+            models.AssignmentSolution.create(howsolved=self.howsolved, assignment=self.assignment, user=request.user)
+
+        response_data = {}
+        response_data['success'] = 'True'
+        return HttpResponse(json.dumps(response_data), content_type='application/json')
