fd8766044a0e7ab966a7f20a59cc97eaf0efbc60
Espen Angell Kristiansen <post@espenak.net>
2014-06-30 17:17:59 +0200

    trix_student.urls: Added login_required to howsolved api.

--- trix/trix_student/urls.py
+++ trix/trix_student/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
 from trix.trix_student.views import dashboard
 from trix.trix_student.views import assignments
@@ -9,7 +9,8 @@ from trix.trix_student.views import howsolved
 urlpatterns = patterns('trix',
     url('^$', dashboard.CourseListView.as_view(), name='trix_student_dashboard' ),
     url('^assignments/$', assignments.AssignmentListView.as_view(), name='trix_student_assignments'),
-    url('^assignment/howsolved$', howsolved.HowsolvedView.as_view(), name='trix_student_howsolved'),
+    url('^assignment/howsolved$', login_required(howsolved.HowsolvedView.as_view()),
+        name='trix_student_howsolved'),
     url('^course/(?P<course_id>\d+)$', course.CourseDetailView.as_view(), name='trix_student_course'),
     url(r'^login$', 'trix_student.views.login.loginview', name='trix-login'),
     url(r'^logout$', 'trix_student.views.logout.logoutview', name='trix-logout'),
