4a37e1ef24ddbb858f508053285a22eabf48ba87
Tor Ivar Johansen <toij@ifi.uio.no>
2014-06-30 18:36:19 +0200

    trix_student: Fixed bug

--- trix/trix_student/views/howsolved.py
+++ trix/trix_student/views/howsolved.py
@@ -12,19 +12,19 @@ class HowsolvedView(View):
     http_method_names = ['post']
 
     def post(self, request, *args, **kwargs):
-        howsolved = request.POST.get('howsolved', None)
+        self.howsolved = request.POST.get('howsolved', None)
         self.assignment_id = request.POST.get('assignment_id', None)
 
         self.assignment = get_object_or_404(models.Assignment, id=self.assignment_id)
-        self.assignment_solution = self.assignment.assignment_set\
+        self.assignment_solution = self.assignment.assignmentsolution_set\
                 .filter(id=self.assignment.id)\
                 .filter(user=request.user)
 
         if self.assignment_solution:
-            self.assignment_solution.howsolved = self.howsolved
-            self.assignment_solution.save()
+            self.assignment_solution[0].howsolved=self.howsolved
+            self.assignment_solution[0].save()
         else:
-            models.AssignmentSolution.create(howsolved=self.howsolved, assignment=self.assignment, user=request.user)
+            self.assignment_solution.create(howsolved=self.howsolved, assignment=self.assignment, user=request.user)
 
         response_data = {}
         response_data['success'] = 'True'
