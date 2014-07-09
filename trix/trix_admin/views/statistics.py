8b0374c29bc16cdefba1337fde1ba83a284abfc6
Tor Ivar Johansen <toij@ifi.uio.no>
2014-07-09 15:30:29 +0200

    simple list of all tags

--- trix/trix_admin/views/statistics.py
+++ trix/trix_admin/views/statistics.py
@@ -1,11 +1,33 @@
 from django.views.generic import TemplateView
 
 from django_cradmin import crapp
+from django_cradmin.viewhelpers import objecttable
 
+from trix.trix_core import models as trix_models
 
-class StatisticsView(TemplateView):
-    template_name = 'trix_admin/frontpage.django.html'
+# class PermalinkListView(objecttable.ObjectTableView):
+#     model = trix_models.Permalink
+#     columns = [
+#         TitleColumn,
+#         TagsColumn,
+#         DescriptionIntroColumn
+#     ]
 
+#     def get_queryset_for_role(self, course):
+#         return self.model.objects.filter(course=course)
+
+class TitleColumn(objecttable.PlainTextColumn):
+    modelfield = 'tag'
+
+class StatisticsView(objecttable.ObjectTableView):
+    model = trix_models.Tag
+  
+    columns = [
+        TitleColumn,
+    ]
+
+    def get_queryset_for_role(self, course):
+        return self.model.objects.all()
 
 
 class App(crapp.App):
