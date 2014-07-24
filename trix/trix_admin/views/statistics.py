019698576f837695048cd7976f46ea89310dd874
Tor Ivar Johansen <tor@torjohansen.no>
2014-07-24 20:44:54 +0200

    Initial csv writer

--- trix/trix_admin/views/statistics.py
+++ trix/trix_admin/views/statistics.py
@@ -1,10 +1,12 @@
 from django.views.generic import ListView
+from django.views.generic import View
 from django.contrib.auth import get_user_model
-
+from django.http import HttpResponse
 from django_cradmin import crapp
 from django_cradmin.viewhelpers import objecttable
 
 from trix.trix_core import models as trix_models
+import csv
 
 # class PermalinkListView(objecttable.ObjectTableView):
 #     model = trix_models.Permalink
@@ -16,7 +18,24 @@ from trix.trix_core import models as trix_models
 
 #     def get_queryset_for_role(self, course):
 #         return self.model.objects.filter(course=course)
+class AssignmentStatsCsv(View):
 
+    def get(self, request, *args, **kwargs):
+        queryset = trix_models.Assignment.objects.filter(tags__id=1)
+        try:
+            fileobj = open('test.csv', 'wb')
+            csvwriter = csv.writer(fileobj, dialect='excel')
+            for assignment in queryset:
+                csvwriter.writerow([assignment.title])
+                csvwriter.writerow(['solved by themself', 10])
+                csvwriter.writerow(['solved by help', 10])
+                csvwriter.writerow(['notsolved', 10])
+                csvwriter.writerow([''])
+        except Exception, e:
+            raise e
+        finally:
+            fileobj.close()
+        return HttpResponse('Hei')
 
 class StatisticsChartView(ListView):
     """
@@ -62,4 +81,5 @@ class App(crapp.App):
     appurls = [
         crapp.Url(r'^$', StatisticsView.as_view(), name=crapp.INDEXVIEW_NAME),
         crapp.Url(r'^view/(?P<pk>\d+)$', StatisticsChartView.as_view(), name='view'),
+        crapp.Url(r'^view/ascsv$', AssignmentStatsCsv.as_view(), name='stats_ascsv'),
     ]
