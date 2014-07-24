a4fea6b3a3e0e4d570244ed716a2b8f9c104ab46
Espen Angell Kristiansen <post@espenak.net>
2014-07-25 00:17:56 +0200

    trix_core.multiassignment_serialize: Working update existing assignments
    from imported YAML dump. Includes validation. See #4.

--- trix/trix_core/multiassignment_serialize.py
+++ trix/trix_core/multiassignment_serialize.py
@@ -1,5 +1,11 @@
 import collections
 import yaml
+from django.db import transaction
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
+from django import forms
+
+from trix.trix_core import models as coremodels
 
 
 class MarkdownString(object):
@@ -68,3 +74,199 @@ def serialize(assignments):
             serializable_assignment['solution'] = MarkdownString(assignment.solution)
         serializable_assignments.append(serializable_assignment)
     return yaml.safe_dump_all(serializable_assignments)
+
+
+class DeserializerError(Exception):
+    """
+    Raised by :class:`.Deserializer` on errors.
+    """
+
+
+class DeserializerDuplicateIdError(DeserializerError):
+    """
+    Raised by :class:`.Deserializer` when multiple assignments
+    with the same ID is encountered.
+    """
+
+
+class DeserializerNotFoundError(DeserializerError):
+    """
+    Raised by :class:`.Deserializer` when we encounter IDs
+    that does not have a corresponding Assignment stored in
+    the database.
+    """
+
+
+class DeserializerSingleValidationError(DeserializerError):
+    """
+    Used to store info about a ``django.core.exceptions.ValidationError``
+    and the data causing the error when we validate in :class:`.Deserializer`.
+    We collect the validation errors as objects of this class, and raise a
+    :exc:`.DeserializerValidationErrors` containing a list of all the
+    DeserializerSingleValidationError objects.
+    """
+    def __init__(self, errordict, data):
+        self.errordict = errordict
+        self.data = data
+
+    def __str__(self):
+        assignmentdict = {'id': self.data.get('id'), 'title': self.data.get('title')}
+        return '{!r}: {!r}'.format(assignmentdict, self.errordict)
+
+
+class DeserializerValidationErrors(DeserializerError):
+    """
+    See :exc:`DeserializerSingleValidationError`.
+    """
+    def __init__(self, errors):
+        """
+        Parameters:
+            errors: List of :class:`.DeserializerSingleValidationError`.
+        """
+        self.errors = errors
+
+    def __str__(self):
+        strerrors = [str(error) for error in self.errors]
+        return str(strerrors)
+
+
+class ListField(forms.Field):
+    def __init__(self, *args, **kwargs):
+        super(ListField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        return value or []
+
+    def validate(self, value):
+        super(ListField, self).validate(value)
+        if not isinstance(value, list):
+            raise ValidationError(_('Invalid value. Must be a list.'), code='invalid')
+        for item in value:
+            if not isinstance(item, basestring):
+                raise ValidationError(_('Invalid value in list. Must be a list of strings.'), code='invalid')
+
+
+class AssignmentDataForm(forms.ModelForm):
+    tags = ListField(required=False)
+
+    class Meta:
+        model = coremodels.Assignment
+        fields = ['title', 'text', 'solution']
+
+
+class Deserializer(object):
+    """
+    Deserialize assignments serialized using :func:`.serialize`.
+
+    If a deserialized assignment has an ``id``, we assume it already exists
+    in the database. If it does not have an ``id``, we assume that the user
+    wants to create a new assignment.
+    """
+
+    def __init__(self, serialized_assignments, coursetag):
+        """
+        Parameters:
+            serialized_assignments: Assignments as serialized by :func:`serialize`.
+            coursetag: A tag to add even if it is not on the given assignments.
+                Only assignments with this tag already in the database is allowed
+                to be updated.
+        """
+        self._deserialize(serialized_assignments)
+        self.coursetag = coursetag
+
+    def _deserialize(self, serialized_assignments):
+        self.deserialized_assignments_with_id = {}
+        self.deserialized_assignments_without_id = []
+        for assignmentdict in yaml.safe_load_all(serialized_assignments):
+            if 'id' in assignmentdict:
+                if assignmentdict['id'] in self.deserialized_assignments_with_id:
+                    raise DeserializerDuplicateIdError(
+                        _('More than one assignment with "id=%(id)s".') % {'id': assignmentdict['id']})
+                else:
+                    self.deserialized_assignments_with_id[assignmentdict['id']] = assignmentdict
+            else:
+                self.deserialized_assignments_without_id.append(assignmentdict)
+
+    def _get_existing_assignments(self):
+        existing_assignments = coremodels.Assignment.objects\
+            .filter(
+                id__in=self.deserialized_assignments_with_id.keys(),
+                tags__tag=self.coursetag)
+        if len(existing_assignments) != len(self.deserialized_assignments_with_id):
+            raise DeserializerNotFoundError(
+                _('One or more of the given IDs was not found among the assignments '
+                  'accessible by "%(coursetag)s".') % {'coursetag': self.coursetag})
+        return existing_assignments
+
+    def _validate_assignment(self, assignment, updated_data):
+        """
+        Validate the given data, apply it to the given assignment, and
+        return the list of tags (list of strings).
+
+        Does not save the assignment.
+        """
+        form = AssignmentDataForm(updated_data)
+        if form.is_valid():
+            assignment.title = form.cleaned_data['title']
+            assignment.text = form.cleaned_data['text']
+            assignment.solution = form.cleaned_data['solution']
+            tags = form.cleaned_data['tags']
+            return tags
+        else:
+            raise DeserializerSingleValidationError(
+                errordict=form.errors, data=updated_data)
+
+    @transaction.atomic
+    def _update_existing_assignments(self):
+        AssignmentTag = coremodels.Assignment.tags.through
+        updated_assignments = []
+        validationerrors = []
+        existing_assignments = self._get_existing_assignments()
+        assignments_by_tag = {}
+
+        for assignment in existing_assignments:
+            updated_data = self.deserialized_assignments_with_id[assignment.id]
+            try:
+                tags = self._validate_assignment(assignment, updated_data)
+            except DeserializerSingleValidationError as e:
+                validationerrors.append(e)
+            else:
+                # Add the assignment to assignments_by_tag
+                # - used below to bulk create the tags
+                for tag in tags:
+                    if tag != self.coursetag:  # We skip the coursetag - we add all assignments to it
+                        if tag not in assignments_by_tag:
+                            assignments_by_tag[tag] = []
+                        assignments_by_tag[tag].append(assignment)
+                if self.coursetag not in assignments_by_tag:
+                    assignments_by_tag[self.coursetag] = []
+                assignments_by_tag[self.coursetag].append(assignment)
+
+        if validationerrors:
+            raise DeserializerValidationErrors(validationerrors)
+        for assignment in existing_assignments:
+            assignment.save()
+
+        # Clear the tags
+        AssignmentTag.objects\
+            .filter(assignment__in=existing_assignments)\
+            .delete()
+
+        # Bulk create any missing tags
+        existing_tags = coremodels.Tag.objects.filter(tag__in=assignments_by_tag.keys())
+        new_tags = set(assignments_by_tag.keys())
+        new_tags.difference_update(set([tagobject.tag for tagobject in existing_tags]))
+        new_tagobjects = [coremodels.Tag(tag=tag) for tag in new_tags]
+        coremodels.Tag.objects.bulk_create(new_tagobjects)
+
+        # Bulk add tags to the assignments
+        assignmenttags = []
+        for tagobject in coremodels.Tag.objects.filter(tag__in=assignments_by_tag.keys()):
+            assignments = assignments_by_tag[tagobject.tag]
+            for assignment in assignments:
+                assignmenttags.append(AssignmentTag(tag=tagobject, assignment=assignment))
+        AssignmentTag.objects.bulk_create(assignmenttags)
+
+        updated_assignments = coremodels.Assignment.objects.filter(
+            id__in=self.deserialized_assignments_with_id.keys())
+        return updated_assignments
--- trix/trix_core/tests/test_multiassignment_serialize.py
+++ trix/trix_core/tests/test_multiassignment_serialize.py
@@ -1,12 +1,12 @@
 import yaml
-from django.test import TestCase
-from trix.trix_core.models import Assignment
+from django.test import TransactionTestCase
+from trix.trix_core import models as coremodels
 from trix.trix_core import multiassignment_serialize
 
 
-class TestMultiassignmentSerialize(TestCase):
+class TestMultiassignmentSerialize(TransactionTestCase):
     def test_yaml_load_sanity(self):
-        data = list(yaml.load_all("""
+        data = list(yaml.safe_load_all("""
 ---
 title: Test One
 tags:
@@ -34,21 +34,79 @@ text: Testtext
             'text': 'Testtext'
         })
 
+    def test_deserializer_grouped_correctly(self):
+        deserializer = multiassignment_serialize.Deserializer(yaml.safe_dump_all([
+            {'id': 1, 'title': 'Existing1'},
+            {'title': 'New'},
+            {'id': 2, 'title': 'Existing2'},
+        ]), coursetag='duck1000')
+        self.assertEquals(
+            deserializer.deserialized_assignments_with_id,
+            {1: {'id': 1, 'title': 'Existing1'}, 2: {'id': 2, 'title': 'Existing2'}}
+        )
+        self.assertEquals(
+            deserializer.deserialized_assignments_without_id,
+            [{'title': 'New'}]
+        )
+
+    def test_deserializer_update_existing_assignments(self):
+        assignment1 = coremodels.Assignment.objects.create(
+            title='Existing1', text='text1')
+        assignment2 = coremodels.Assignment.objects.create(
+            title='Existing2', text='text2')
+        duck1000tag = coremodels.Tag.objects.create(tag='duck1000')
+        assignment1.tags.add(duck1000tag)
+        assignment2.tags.add(duck1000tag)
+        deserializer = multiassignment_serialize.Deserializer(yaml.safe_dump_all([
+            {'id': assignment1.id, 'title': 'Updated1', 'text': 'updatedText1',
+             'tags': ['oblig1']},
+            {'title': 'New'},
+            {'id': assignment2.id, 'title': 'Updated2', 'text': 'updatedText2',
+             'tags': ['duck1000', 'oblig2', 'week3']},
+        ]), coursetag='duck1000')
+        updated_assignments = deserializer._update_existing_assignments()
+        self.assertEquals(set(updated_assignments), set([assignment1, assignment2]))
+        assignment1 = coremodels.Assignment.objects.get(id=assignment1.id)
+        assignment2 = coremodels.Assignment.objects.get(id=assignment2.id)
+        self.assertEquals(assignment1.title, 'Updated1')
+        self.assertEquals(assignment2.title, 'Updated2')
+        self.assertEquals(
+            set([tagobject.tag for tagobject in assignment1.tags.all()]),
+            set(['duck1000', 'oblig1']))
+        self.assertEquals(coremodels.Assignment.objects.count(), 2)  # Nothing extra created
+
+    def test_deserializer_update_existing_assignments_invalid(self):
+        assignment1 = coremodels.Assignment.objects.create(
+            title='Existing1', text='text1')
+        assignment1.tags.create(tag='duck1000')
+        deserializer = multiassignment_serialize.Deserializer(yaml.safe_dump_all([
+            {'id': assignment1.id, 'title': 'Updated1'},
+        ]), coursetag='duck1000')
+        with self.assertRaises(multiassignment_serialize.DeserializerValidationErrors):
+            deserializer._update_existing_assignments()
+        assignment1 = coremodels.Assignment.objects.get(id=assignment1.id)
+        self.assertEquals(assignment1.title, 'Existing1')
+        self.assertEquals(
+            set([tagobject.tag for tagobject in assignment1.tags.all()]),
+            set(['duck1000']))
+        self.assertEquals(coremodels.Assignment.objects.count(), 1)  # Nothing extra created
+
     def test_serialize_assignments_single(self):
-        assignment = Assignment.objects.create(
+        assignment = coremodels.Assignment.objects.create(
             title='A title',
             text='A text'
         )
         self.assertEquals(
             multiassignment_serialize.serialize([assignment]),
-            ('id: 1\n'
+            ('id: {}\n'
              'title: A title\n'
              'tags: []\n'
              'text: |-\n'
-             '  A text\n'))
+             '  A text\n').format(assignment.id)
+        )
 
     def test_serialize_assignments_single_with_tags(self):
-        assignment = Assignment.objects.create(
+        assignment = coremodels.Assignment.objects.create(
             title='A title',
             text='A text'
         )
@@ -56,24 +114,51 @@ text: Testtext
         assignment.tags.create(tag='testtag2')
         self.assertEquals(
             multiassignment_serialize.serialize([assignment]),
-            ('id: 1\n'
+            ('id: {}\n'
              'title: A title\n'
              'tags: [testtag, testtag2]\n'
              'text: |-\n'
-             '  A text\n'))
+             '  A text\n').format(assignment.id)
+        )
 
     def test_serialize_assignments_single_with_solution(self):
-        assignment = Assignment.objects.create(
+        assignment = coremodels.Assignment.objects.create(
             title='A title',
             text='A text',
             solution='A solution'
         )
         self.assertEquals(
             multiassignment_serialize.serialize([assignment]),
-            ('id: 1\n'
+            ('id: {}\n'
              'title: A title\n'
              'tags: []\n'
              'text: |-\n'
              '  A text\n'
              'solution: |-\n'
-             '  A solution\n'))
+             '  A solution\n').format(assignment.id)
+        )
+
+    def test_serialize_assignments_multi(self):
+        assignment1 = coremodels.Assignment.objects.create(
+            title='A1',
+            text='text1'
+        )
+        assignment2 = coremodels.Assignment.objects.create(
+            title='A2',
+            text='text2'
+        )
+        self.assertEquals(
+            multiassignment_serialize.serialize([assignment1, assignment2]),
+            ('id: {}\n'
+             'title: A1\n'
+             'tags: []\n'
+             'text: |-\n'
+             '  text1\n'
+             '---\n'
+             'id: {}\n'
+             'title: A2\n'
+             'tags: []\n'
+             'text: |-\n'
+             '  text2\n'
+             ).format(assignment1.id, assignment2.id)
+        )
