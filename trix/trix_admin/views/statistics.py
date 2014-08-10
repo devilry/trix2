1634001c4cdd45d0c2fa893f0a1470100ce58ed8
Tor Ivar Johansen <tor@torjohansen.no>
2014-08-10 20:04:00 +0200

    trix_admin: Fixed utf-8 encoding issues

--- trix/trix_admin/views/statistics.py
+++ trix/trix_admin/views/statistics.py
@@ -9,6 +9,41 @@ from django_cradmin.viewhelpers import objecttable
 
 from trix.trix_core import models as trix_models
 import csv
+import codecs
+import cStringIO
+
+
+class UnicodeWriter:
+    """
+
+    Picked from https://docs.python.org/2/library/csv.html#examples
+
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
 
 
 class AssignmentStatsCsv(View):
@@ -45,9 +80,9 @@ class AssignmentStatsCsv(View):
         response = HttpResponse(content_type='text/csv')
         try:
             response['Content-Disposition'] = 'attachment; filename="trix-statistics.csv"'
-            csvwriter = csv.writer(response, dialect='excel')
+            csvwriter = UnicodeWriter(response)  # csv.writer(response, dialect='excel')
             csvwriter.writerow([_('Simple statitics showing percentage share of how the assignments where solved')])
-            csvwriter.writerow([_('Total number of users'), user_count])
+            csvwriter.writerow([_('Total number of users'), str(user_count)])
             csvwriter.writerow('')
             csvwriter.writerow([_('Assignment title'), _('Percentage')])
             for assignment in queryset:
