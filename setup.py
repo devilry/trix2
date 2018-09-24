462d15fa61010640895d729cf913ac6832c5557c
Jonas <Elthan@users.noreply.github.com>
2018-09-24 12:35:23 +0200

    Added unicodecsv to setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'dj-database-url>=0.5.0',
         'cradmin_legacy>=1.3.0a0',
         'gunicorn',
+        'unicodecsv>=0.14.1',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
