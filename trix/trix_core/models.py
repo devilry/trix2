f68f01d553bd938b0d72b904fbf7523620aa0627
Jonas Sandbekk <shaitan25@gmail.com>
2019-06-06 13:13:37 +0200

    Fixed tests

--- trix/trix_core/models.py
+++ trix/trix_core/models.py
@@ -3,7 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
-from django.utils.encoding import python_2_unicode_compatible
 
 
 class TrixUserManager(BaseUserManager):
