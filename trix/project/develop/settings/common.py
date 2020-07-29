fd875ef96c1d4e66db501a7994fa142309fa727e
Tor Ivar Johansen <tor@torjohansen.no>
2020-07-29 12:11:42 +0200

    Re-enable postgres as development database config

--- trix/project/develop/settings/common.py
+++ trix/project/develop/settings/common.py
@@ -2,8 +2,7 @@
 Common development settings.
 """
 from trix.project.default.settings import *             # noqa
-from trix.project.default.settings import INSTALLED_APPS, MIDDLEWARE
-# from django_dbdev.backends.postgres import DBSETTINGS
+from django_dbdev.backends.postgres import DBSETTINGS
 
 DEBUG = True
 ROOT_URLCONF = 'trix.project.develop.urls'
@@ -13,11 +12,11 @@ INSTALLED_APPS = list(INSTALLED_APPS) + [
     # 'debug_toolbar',
 ]
 
-# DATABASES = {
-#     'default': DBSETTINGS
-# }
+DATABASES = {
+    'default': DBSETTINGS
+}
 
-# DATABASES['default']['PORT'] = 20987
+DATABASES['default']['PORT'] = 20987
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
