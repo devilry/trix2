5a4d8428628b1366b1ae7b5302feeaba83ac8db9
Levijatan <aasmund@appresso.no>
2022-11-21 15:34:11 +0100

    Added migration to populate UserEmail Objects

--- trix/trix_auth/urls.py
+++ trix/trix_auth/urls.py
@@ -1,32 +1,18 @@
-from django.conf import settings
 from django.conf.urls import include, url
-from django.contrib.auth import views as auth_views
+from trix.trix_auth.views import allauth_views, login
 
-
-if settings.DEBUG:
-    from trix.trix_auth.views import login
-    urlpatterns = [
-        url('^login/$',
-            login.TrixLoginView.as_view(),
-            name='trix_login'),
-        url('^logout/$',
-            auth_views.LogoutView.as_view(),
-            name='trix_logout'),
-    ]
-else:
-    from trix.trix_auth.views import allauth_views, login
-    urlpatterns = [
-        url('^login/$',
-            login.TrixLoginView.as_view(),
-            name='trix_login'),
-        url('^logout/$',
-            allauth_views.AllauthLogoutView.as_view(),
-            name='trix_logout'),
-        url('^allauth/login/$',
-            allauth_views.AllauthLoginView.as_view(),
-            name='account_login'),
-        url('^allauth/logout/$',
-            allauth_views.AllauthLogoutView.as_view(),
-            name='account_logout'),
-        url('^allauth/', include('allauth.urls')),
-    ]
+urlpatterns = [
+    url('^login/$',
+        login.TrixLoginView.as_view(),
+        name='trix_login'),
+    url('^logout/$',
+        allauth_views.AllauthLogoutView.as_view(),
+        name='trix_logout'),
+    url('^allauth/login/$',
+        allauth_views.AllauthLoginView.as_view(),
+        name='account_login'),
+    url('^allauth/logout/$',
+        allauth_views.AllauthLogoutView.as_view(),
+        name='account_logout'),
+    url('^allauth/', include('allauth.urls')),
+]
