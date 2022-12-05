c92a33cc54e1a2efa973094005cf421ac52f8e17
Levijatan <aasmund@appresso.no>
2022-12-05 15:26:12 +0100

    wcag now defaults to activated

--- trix/trix_student/views/base.py
+++ trix/trix_student/views/base.py
@@ -8,6 +8,7 @@ class TrixListViewBase(ListView):
     This forms the basis for all other list views and is used to apply sitewide (limited to user
     sites) context.
     '''
+
     def get(self, request, **kwargs):
         # Fix out of bounds pagination
         page = request.GET.get('page')
@@ -22,7 +23,7 @@ class TrixListViewBase(ListView):
     def get_context_data(self, **kwargs):
         context = super(TrixListViewBase, self).get_context_data(**kwargs)
         # Check if WCAG styles have been activated
-        context['wcag'] = self.request.session.get('wcag', False)
+        context['wcag'] = self.request.session.get('wcag', True)
         return context
 
 
@@ -30,7 +31,7 @@ def wcag_change(request):
     '''
     Turn the WCAG colours on or off.
     '''
-    wcag = request.session.get("wcag", False)
+    wcag = request.session.get("wcag", True)
     wcag = not wcag
     request.session["wcag"] = wcag
     return redirect('trix_student_dashboard')
